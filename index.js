2d890ef032370e7112a47032ad0118ca7771ab33
Dianna <dianna.yim@gmail.com>
2018-02-08 13:33:15 -0700

    last push

--- index.js
+++ index.js
@@ -2,12 +2,4 @@
 
 $(document).ready(function() {
 	"use strict";
-	
-	$("#whoAreWe").scroll(function() {
-		var pos = $('#whoAreWe').scrollTop();
-		console.log(pos);
-		if (pos === 0) {
-			alert('top of the div');
-		}
-	});
 });
